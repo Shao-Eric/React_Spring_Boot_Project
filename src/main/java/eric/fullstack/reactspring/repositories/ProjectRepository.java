d5476b18f14837ec6e37cf4e492bdee4d26eaf72
Shao Eric Hu <erichu11364@gmail.com>
2019-02-11 22:56:05 -0500

    Setting up H2 database

---
+++ src/main/java/eric/fullstack/reactspring/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package eric.fullstack.reactspring.repositories;
+
+import eric.fullstack.reactspring.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    @Override
+    Iterable<Project> findAllById(Iterable<Long> iterable);
+}
