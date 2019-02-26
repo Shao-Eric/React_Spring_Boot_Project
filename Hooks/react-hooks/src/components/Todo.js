6aa3c3cf939ceb715d34d2d21ff146ee231da21c
Shao Eric Hu <erichu11364@gmail.com>
2019-02-25 22:52:12 -0500

    bean scope

---
+++ Hooks/react-hooks/src/components/Todo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const todo = props => {
+    return <React.Fragment>
+        <input type="text" placeholder="Todo" />
+        <button type="button">Add</button>
+        <ul>
+            
+        </ul>
+    </React.Fragment>
+}
+
+export default todo;
