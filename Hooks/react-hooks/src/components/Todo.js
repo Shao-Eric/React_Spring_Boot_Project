1309a81ea56f0891f92fcaa249a813eb2b8dfa27
Shao Eric Hu <erichu11364@gmail.com>
2019-03-02 22:02:52 -0500

    done with bean life cycle

--- Hooks/react-hooks/src/components/Todo.js
+++ Hooks/react-hooks/src/components/Todo.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const todo = props => {
+    const inputState = useState(''); //returns an array
+
     return <React.Fragment>
         <input type="text" placeholder="Todo" />
         <button type="button">Add</button>
         <ul>
-            
+
         </ul>
     </React.Fragment>
 }
--- Spring_Workspace/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
+++ Spring_Workspace/spring-demo-annotations/src/com/luv2code/springdemo/TennisCoach.java
@@ -1,10 +1,15 @@
 package com.luv2code.springdemo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("singleton")
 public class TennisCoach implements Coach {
 	
 	@Autowired
@@ -15,6 +20,18 @@ public class TennisCoach implements Coach {
 	
 	public TennisCoach() {
 		System.out.println(">> inside default constructor");
+	}
+	
+	// Define my init method
+	@PostConstruct
+	public void doMyStartUpStuff() {
+		System.out.println(">> TennisCoach: Inside of doMyStartUpStuff()");
+	}
+	
+	// Define my destroy method
+	@PreDestroy
+	public void doMyCleanupStuff() {
+		System.out.println(">> TennisCoach: inside of doMyCleanupStuff()");
 	}
 	
 	
