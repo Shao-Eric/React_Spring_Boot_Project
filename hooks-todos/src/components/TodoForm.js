f2d8d13a138873aadbc799615b6c2546854681e1
Shao Eric Hu <erichu11364@gmail.com>
2019-04-07 16:55:40 -0400

    finishing up

---
+++ hooks-todos/src/components/TodoForm.js
@@ -0,0 +1,39 @@
+import React, { useState, useContext, useEffect } from "react";
+import TodosContext from "../context";
+
+export default function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const {
+    state: { currentTodo = {} },
+    dispatch
+  } = useContext(TodosContext);
+
+  useEffect(() => {
+    if (currentTodo.text) {
+      setTodo(currentTodo.text);
+    } else {
+      setTodo("");
+    }
+  }, [currentTodo.id]);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (currentTodo.text) {
+      dispatch({ type: "UPDATE_TODO", payload: todo });
+    } else {
+      dispatch({ type: "ADD_TODO", payload: todo });
+    }
+    setTodo("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex justify-content p-5">
+      <input
+        type="text"
+        className="border-black border-solid border-2"
+        value={todo}
+        onChange={event => setTodo(event.target.value)}
+      />
+    </form>
+  );
+}
