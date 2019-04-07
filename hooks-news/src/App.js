26c199f401f526982da7505eee69a099f9402616
Shao Eric Hu <erichu11364@gmail.com>
2019-04-07 16:59:06 -0400

    ading hook news

--- hooks-news/src/App.js
+++ hooks-news/src/App.js
@@ -1,5 +1,88 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 export default function App() {
-  return <div>App</div>;
+  const [results, setResults] = useState([]);
+  const [query, setQuery] = useState("react hooks");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
+
+  // useEffect runs after every render
+  useEffect(() => {
+    console.log("useEffect called");
+    getResults();
+  }, []);
+
+  const getResults = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setResults(response.data.hits);
+    } catch (err) {
+      setError(err);
+      console.log(err);
+    }
+    setLoading(false);
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+  const handleClearSearch = () => {
+    setQuery("");
+    console.log(searchInputRef);
+    searchInputRef.current.focus();
+  };
+
+  return (
+    <div className="container max-w-md mx-auto p-4 m-2 bg-purple-lightest shadow-lg rounded">
+      <img
+        src="https://icon.now.sh/react/coc"
+        alt="#"
+        className="float-right h-12"
+      />
+      <h1 className="text-grey-darkest font-thin">Hooks News</h1>
+      <form className="mb-2" onSubmit={handleSearch}>
+        <input
+          type="text"
+          onChange={event => setQuery(event.target.value)}
+          value={query}
+          ref={searchInputRef}
+          className="border p-1 rounded"
+        />
+        <button className="bg-orange rounded m-1 p-1" type="submit">
+          Search
+        </button>
+        <button
+          className="bg-teal text-white rounded p-1"
+          type="button"
+          onClick={handleClearSearch}
+        >
+          Clear
+        </button>
+      </form>
+      {loading ? (
+        <div className="font-bold text-orange-dark">Loading results...</div>
+      ) : (
+        <ul className="list-reset leading-normal">
+          {results.map((result, index) => (
+            <li key={result.objectID}>
+              <a
+                className="text-indigo-dark hover:text-indigo-darkest"
+                href={result.url}
+              >
+                {result.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {error && <div className="text-red font-bold">{error.message}</div>}
+    </div>
+  );
 }
